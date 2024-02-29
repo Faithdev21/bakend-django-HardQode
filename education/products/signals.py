af9b56a59c1045608710a33b868239881ea07c71
Егор Лоскутов <egorkaloskutov@yandex.ru>
2024-02-29 19:25:47 +0300

    Removed unneccessary prints

--- education/products/signals.py
+++ education/products/signals.py
@@ -10,10 +10,8 @@ access_granted = Signal()
 
 @receiver(access_granted)
 def on_access_granted(sender, product, **kwargs):
-    print("Сигнал доступа получен")
     try:
         if product.start_date > timezone.now():
-            print("Запуск распределения пользователей по группам")
             distribute_users_to_groups(product)
     except Group.DoesNotExist:
         raise ValidationError("Все группы заполнены.")
